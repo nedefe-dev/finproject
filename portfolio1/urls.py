4fed98528834cbee2efe5e92189193d0a997f8f8
ned efe <ned.efe@gmail.com>
2020-02-10 17:29:53 +1100

    some updates

--- portfolio1/urls.py
+++ portfolio1/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), # Includes site authentication URLs
+
 ]
--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from projects import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('', views.all_projects),
